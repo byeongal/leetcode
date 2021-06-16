e275fc7571825871cf1091ad02b090257d17e430
YoungJae <byeongal@comcom.ai>
2021-06-16 23:11:40 +0900

    capacity-to-ship-packages-within-d-days.py

---
+++ Easy/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left:int = max(weights)
+        right:int = sum(weights)
+        answer:int = right
+        while left <= right:
+            mid:int = (left + right) // 2
+            needs:int = 1
+            accumulated_weight:int = 0
+            for weight in weights:
+                if accumulated_weight + weight > mid:
+                    needs += 1
+                    accumulated_weight = 0
+                accumulated_weight += weight
+            if needs > days:
+                left = mid + 1
+            else:
+                right = mid - 1
+                answer = mid
+        return answer
