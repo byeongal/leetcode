19372fe795d75c82ef0c105f1db04d10a2e7818a
youngjae <byeongal@comcom.ai>
2022-02-17 18:16:12 +0900

    solved

---
+++ Medium/search-a-2d-matrix.py
@@ -0,0 +1,22 @@
+from typing import *
+
+
+def find(matrix: List[List[int]], area: List[int], target: int) -> bool:
+    left_up_y, left_up_x, right_down_y, right_down_x = area
+    if left_up_y > right_down_y or left_up_x > right_down_x:
+        return False
+    mid_y = (left_up_y + right_down_y) // 2
+    mid_x = (left_up_x + right_down_x) // 2
+    if matrix[mid_y][mid_x] == target:
+        return True
+    if matrix[mid_y][mid_x] < target:
+        return find(matrix, [mid_y, mid_x + 1, mid_y, right_down_x], target) or \
+               find(matrix, [mid_y + 1, left_up_x, right_down_y, right_down_x], target)
+    else:
+        return find(matrix, [mid_y, left_up_x, mid_y, mid_x - 1], target) or \
+               find(matrix, [left_up_y, left_up_x, mid_y - 1, right_down_x], target)
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return find(matrix, [0, 0, len(matrix) - 1, len(matrix[0]) - 1], target)
